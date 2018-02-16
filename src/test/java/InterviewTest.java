b68a40b02e870ae10d990e9f50ee1d5782996773
Paul Fechete <paul.fechete@paddypowerbetfair.com>
2018-02-16 11:50:56 +0200

    Minor changes

--- src/test/java/InterviewTest.java
+++ src/test/java/InterviewTest.java
@@ -6,17 +6,31 @@ import java.net.URL;
 public class InterviewTest {
 
     public static void main(String[] args) {
-        URL geckoDriver = InterviewTest.class.getResource("/geckodriver.exe");
-        System.setProperty("webdriver.gecko.driver", geckoDriver.getPath());
+        URL chromeDriver = InterviewTest.class.getResource("/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", chromeDriver.getPath());
 
-        //Instantiate a new webdriver
+        //1. Instantiate a new webdriver of type ChromeDriver
 
-        //Navigate to "https://promos.betfair.com"
+        //2. Navigate to "https://promos.betfair.com"
 
-        //Click on Arcade in the black subnavigation bar
+        //3. Click on Arcade in the black subnavigation bar
 
-        //Click on second promotion
+        //4. Surround click of element with try catch, catch NoSuchElement exception and print exception message.
 
-        //Verify the name of the promotion
+        //5. Get list of all promotions on page and click on second promotion   (Can you write this using java 8?)
+
+        //6. Verify the name of the promotion
+
+        //7. Get the name of the promotion
+
+        //8. Create a new java List of Strings and add promotion name to list
+
+        //9. Iterate over list and print list elements
     }
+
+    //10. Write a test for the above, with before and after
+
+    //11. Write a data provider for the test
+
+    //12. Write an enum for all the values in the subnav and use it in the data provider to replace step 3
 }
